d4b60234bea555beb3bdbb5e1fb3d6d8aa624a1a
Venki8500 <167505277+Venki8500@users.noreply.github.com>
2024-10-30 11:17:29 +0530

    Add files via upload

---
+++ JSX Practice/players.js
@@ -0,0 +1,192 @@
+var players = [
+
+    {
+"firstname": "Virat",
+"lastname": "Kohli",
+"age": 35,
+"gender": "Male",
+"place": "Delhi",
+"date_of_birth": "1988-11-05",
+"centuries": 75
+},
+{
+"firstname": "Rohit",
+"lastname": "Sharma",
+"age": 37,
+"gender": "Male",
+"place": "Bansod",
+"date_of_birth": "1987-04-30",
+"centuries": 44
+},
+{
+"firstname": "Steve",
+"lastname": "Smith",
+"age": 35,
+"gender": "Male",
+"place": "Sydney",
+"date_of_birth": "1989-06-02",
+"centuries": 48
+},
+{
+"firstname": "Kane",
+"lastname": "Williamson",
+"age": 34,
+"gender": "Male",
+"place": "Hudson",
+"date_of_birth": "1990-04-08",
+"centuries": 26
+},
+{
+"firstname": "Joe",
+"lastname": "Root",
+"age": 33,
+"gender": "Male",
+"place": "Sheffield",
+"date_of_birth": "1990-12-30",
+"centuries": 29
+},
+{
+"firstname": "Babar",
+"lastname": "Azam",
+"age": 29,
+"gender": "Male",
+"place": "Lahore",
+"date_of_birth": "1994-10-15",
+"centuries": 19
+},
+{
+"firstname": "David",
+"lastname": "Warner",
+"age": 37,
+"gender": "Male",
+"place": "Sydney",
+"date_of_birth": "1986-10-27",
+"centuries": 25
+},
+{
+"firstname": "AB",
+"lastname": "de Villiers",
+"age": 40,
+"gender": "Male",
+"place": "Pretoria",
+"date_of_birth": "1984-02-17",
+"centuries": 47
+},
+{
+"firstname": "MS",
+"lastname": "Dhoni",
+"age": 43,
+"gender": "Male",
+"place": "Ranchi",
+"date_of_birth": "1981-07-07",
+"centuries": 10
+},
+{
+"firstname": "Shakib",
+"lastname": "Al Hasan",
+"age": 37,
+"gender": "Male",
+"place": "Magura",
+"date_of_birth": "1987-03-24",
+"centuries": 11
+},
+{
+"firstname": "Pat",
+"lastname": "Cummins",
+"age": 31,
+"gender": "Male",
+"place": "Westmead",
+"date_of_birth": "1993-05-08",
+"centuries": 0
+},
+{
+"firstname": "Jasprit",
+"lastname": "Bumrah",
+"age": 30,
+"gender": "Male",
+"place": "Gujarat",
+"date_of_birth": "1993-12-06",
+"centuries": 0
+},
+{
+"firstname": "Yuzvendra",
+"lastname": "Chahal",
+"age": 34,
+"gender": "Male",
+"place": "Jind",
+"date_of_birth": "1990-07-23",
+"centuries": 0
+},
+{
+"firstname": "Rashid",
+"lastname": "Khan",
+"age": 25,
+"gender": "Male",
+"place": "Jalalabad",
+"date_of_birth": "1998-09-20",
+"centuries": 0
+},
+{
+"firstname": "Trent",
+"lastname": "Boult",
+"age": 35,
+"gender": "Male",
+"place": "Rotorua",
+"date_of_birth": "1989-07-22",
+"centuries": 0
+},
+{
+"firstname": "Sunil",
+"lastname": "Narine",
+"age": 35,
+"gender": "Male",
+"place": "Arima",
+"date_of_birth": "1988-05-26",
+"centuries": 0
+},
+{
+"firstname": "Mohammed",
+"lastname": "Shami",
+"age": 34,
+"gender": "Male",
+"place": "Amroha",
+"date_of_birth": "1990-09-03",
+"centuries": 0
+},
+{
+"firstname": "Pat",
+"lastname": "Brown",
+"age": 32,
+"gender": "Male",
+"place": "Wolverhampton",
+"date_of_birth": "1992-11-20",
+"centuries": 0
+},
+{
+"firstname": "Ricky",
+"lastname": "Ponting",
+"age": 49,
+"gender": "Male",
+"place": "Launceston",
+"date_of_birth": "1974-12-19",
+"centuries": 71
+},
+{
+"firstname": "Brian",
+"lastname": "Lara",
+"age": 55,
+"gender": "Male",
+"place": "Santa Cruz",
+"date_of_birth": "1969-05-02",
+"centuries": 53
+},
+{
+"firstname": "Sachin",
+"lastname": "Tendulkar",
+"age": 51,
+"gender": "Male",
+"place": "Mumbai",
+"date_of_birth": "1973-04-24",
+"centuries":100
+}
+]
